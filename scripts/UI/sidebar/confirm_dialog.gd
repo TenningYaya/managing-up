5f4a844c4c6cf593ba155d96170767719c778335
TenningYaya <98125817+TenningYaya@users.noreply.github.com>
2026-05-21 02:05:07 -0400

    修confirm dialog卡互动问题

--- scripts/UI/sidebar/confirm_dialog.gd
+++ scripts/UI/sidebar/confirm_dialog.gd
@@ -1,10 +1,7 @@
-extends Control
+extends CanvasLayer
 
 signal confirmed
 # 当这个对话框显示出来时，我们确保它能接收到点击
-func _ready():
-	# 确保根节点的鼠标过滤是 Stop，这样它才能“感觉到”点击
-	mouse_filter = Control.MOUSE_FILTER_STOP
 
 # --- 1. 点击“确定”：直接退出游戏 ---
 func _on_confirm_button_pressed():
